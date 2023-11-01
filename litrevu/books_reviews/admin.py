552c03245d760b31f4f99fb128de8418c2846fee
my-tricks <silva.jeremie93@gmail.com>
2023-11-01 22:39:20 +0100

    DEV

--- litrevu/books_reviews/admin.py
+++ litrevu/books_reviews/admin.py
@@ -1,3 +1,15 @@
-from django.contrib import admin
-
 # Register your models here.
+
+from django.contrib import admin
+from .models import (
+    CustomUser,
+    Book,
+    Review,
+    Ticket,
+)
+
+
+admin.site.register(CustomUser)
+admin.site.register(Book)
+admin.site.register(Review)
+admin.site.register(Ticket)
