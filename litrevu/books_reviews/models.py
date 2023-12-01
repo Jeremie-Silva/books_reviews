3b52af674083b1571b088da827861b0cc5d38c69
my-tricks <silva.jeremie93@gmail.com>
2023-12-01 14:45:08 +0100

    DEV

--- litrevu/books_reviews/models.py
+++ litrevu/books_reviews/models.py
@@ -67,7 +67,7 @@ class Ticket(models.Model):
 
     @admin.display(boolean=True)
     def answered(self):
-        return True if self.review else False
+        return bool(True if self.review else False)
 
     def __str__(self):
         return self.book.title
--- litrevu/books_reviews/views.py
+++ litrevu/books_reviews/views.py
@@ -18,9 +18,14 @@ def inscription(request):
 @login_required(login_url="login")
 def flux(request):
     user = UserProfile.objects.get(user__username=request.user)
+    flux: list = []
+    for user_follows in user.follows.all():
+        flux += list(user_follows.tickets.all())
+        flux += list(user_follows.reviews.all())
     data = {
-        "tickets": user.tickets.all(),
-        "reviews": user.reviews.all()
+        "username": user.user.username,
+        "flux": sorted(flux, key=lambda x: x.creation_date, reverse=True),
+        "rating_loop": range(5)
     }
     return render(request, template_name="books_reviews/flux.html", context=data)
 
@@ -50,7 +55,6 @@ def review_creation(request):
 def my_posts(request):
     user = UserProfile.objects.get(user__username=request.user)
     personal_posts: list = list(user.tickets.all()) + list(user.reviews.all())
-    sorted(personal_posts, key=lambda x: x.creation_date, reverse=True)
     data = {
         "username": user.user.username,
         "my_posts": sorted(personal_posts, key=lambda x: x.creation_date, reverse=True),
